5adcd3cae7a0d08d2765d51d8afbbbc1f55229a4
Iamvictorsam <iamvictorsam@gmail.com>
2023-01-12 09:50:11 +0100

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:tour_app/tour_target.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final wealthKey = GlobalKey();
+  final profileKey = GlobalKey();
+  final buttonKey = GlobalKey();
+  final pageKey = GlobalKey();
+
+  bool reveal = false;
+
+  late TutorialCoachMark _tutorialCoachMark;
+
+  void _initialPageTour() {
+    _tutorialCoachMark = TutorialCoachMark(
+        targets: addTourTargets(
+            pageKey: pageKey,
+            profileKey: profileKey,
+            wealthKey: wealthKey,
+            buttonKey: buttonKey),
+        colorShadow: Colors.teal,
+        paddingFocus: 10,
+        hideSkip: false,
+        opacityShadow: 0.8,
+        onFinish: () {
+          // The tour finshes here. You can add it to local storage to avoid showing again
+          print('Finished Tour');
+        },
+        onSkip: () {
+          // The tour finshes here. You can add it to local storage to avoid showing again
+          print('Skipped');
+        });
+  }
+
+  void _ShowTour() {
+    Future.delayed(const Duration(seconds: 1),
+        () => _tutorialCoachMark.show(context: context));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initialPageTour();
+    _ShowTour();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff040404),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Text('Home',
+                    style: const TextStyle(color: Colors.white, fontSize: 22),
+                    key: pageKey),
+                CircleAvatar(
+                  key: profileKey,
+                  child: const Icon(Icons.person),
+                ),
+              ]),
+              const Spacer(),
+              const Text('You are Rich',
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
+              const SizedBox(height: 10),
+              Center(
+                child: FlutterLogo(
+                  key: wealthKey,
+                  size: MediaQuery.of(context).size.height * 0.3,
+                ),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                  key: buttonKey,
+                  onPressed: () => setState(() => reveal = !reveal),
+                  child: const Text('Find out More...')),
+              const SizedBox(height: 20),
+              Visibility(
+                visible: reveal,
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  color: Colors.white,
+                  child: const Text('Your Diamond is Worth\n \$34,000,000',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      )),
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/tour_target.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
+
+List<TargetFocus> addTourTargets({
+  required GlobalKey pageKey,
+  required GlobalKey profileKey,
+  required GlobalKey wealthKey,
+  required GlobalKey buttonKey,
+}) {
+  List<TargetFocus> targets = [];
+  targets.add(
+    TargetFocus(
+      keyTarget: pageKey,
+      alignSkip: Alignment.bottomRight,
+      shape: ShapeLightFocus.RRect,
+      radius: 10,
+      contents: [
+        TargetContent(
+          align: ContentAlign.bottom,
+          builder: (context, controller) => Container(
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text('This is The Home Page',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20,
+                    )),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+
+  targets.add(
+    TargetFocus(
+      keyTarget: profileKey,
+      alignSkip: Alignment.bottomLeft,
+      shape: ShapeLightFocus.Circle,
+      radius: 10,
+      contents: [
+        TargetContent(
+          align: ContentAlign.bottom,
+          builder: (context, controller) => Container(
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text('Your Profile Icon',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20,
+                    ))
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+
+  targets.add(
+    TargetFocus(
+      keyTarget: wealthKey,
+      alignSkip: Alignment.topRight,
+      shape: ShapeLightFocus.RRect,
+      radius: 10,
+      contents: [
+        TargetContent(
+          align: ContentAlign.bottom,
+          builder: (context, controller) => Container(
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text('Hey!!\n This is your Worth',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20,
+                    ))
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+
+  targets.add(
+    TargetFocus(
+      keyTarget: buttonKey,
+      alignSkip: Alignment.topRight,
+      shape: ShapeLightFocus.RRect,
+      radius: 10,
+      contents: [
+        TargetContent(
+          align: ContentAlign.bottom,
+          builder: (context, controller) => Container(
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text('Click to find out how much you worth',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ))
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+
+  return targets;
+}
